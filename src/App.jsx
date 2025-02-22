adbce2d63f9325517da62d3d920fc219a1a82629
sreekanth <ch.sreekanth38@gmail.com>
2025-02-22 11:36:43 +0530

    psuh test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold  bg-red-400">NetflixGPT</h1>
+      <h1 className="text-3xl  bg-red-400">NetflixGPT</h1>
     </>
   );
 }
